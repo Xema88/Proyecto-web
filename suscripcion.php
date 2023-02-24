806c99aba93553465dc0868045645bc233dead9f
Xema88 <126241778+Xema88@users.noreply.github.com>
2023-02-24 10:40:49 +0100

    Add files via upload

---
+++ suscripcion.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <p>Nombre: <?php echo $_POST["nombre"]; ?></p><br>
+        <p>Email: <?php echo $_POST["email"]; ?></p><br>
+        <p>Blog personal: <?php echo $_POST["blogPersonal"]; ?></p><br>
+        <p>Fecha de nacimiento: <?php echo $_POST["fechaNacimiento"]; ?></p><br>
+    </body>
+</html>
